ad96449e538b8d248a265e46b992a47579c39119
Armando Tello <ckat609@yahoo.com>
2020-07-15 18:35:41 -0500

    finished modular md

---
+++ MEAN_036_modular_md/server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const session = require('express-session');
+const flash = require('express-flash');
+const mongoose = require('mongoose');
+const app = express();
+
+app.use(express.static(__dirname + '/static'));
+app.use(express.urlencoded({
+    extended: true
+}));
+app.use(flash());
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+app.use(session({
+    secret: "hellokitty",
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        maxAge: 60000
+    }
+}));
+
+mongoose.connect('mongodb://localhost/pack', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+require('./server/config/routes')(app);
+
+app.listen(8000, () => console.log("Listening on port 8000"));
