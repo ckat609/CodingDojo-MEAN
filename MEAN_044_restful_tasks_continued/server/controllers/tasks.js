e36bd68c7e2b3d90708c2854d390c40a74ef09ec
Armando Tello <ckat609@yahoo.com>
2020-07-17 20:39:41 -0500

    finished restful tasks continued

--- MEAN_044_restful_tasks_continued/server/controllers/tasks.js
+++ MEAN_044_restful_tasks_continued/server/controllers/tasks.js
@@ -4,25 +4,32 @@ const {
 
 module.exports = {
     index: function (req, res) {
-        console.log("I'm in the controller!")
         Task.find()
             .then(data => {
                 console.log(data);
-                res.json(data);
+                res.json({
+                    tasks: data
+                });
             })
             .catch(err => res.json(err))
     },
-
     show: function (req, res) {
         Task.findOne({
                 _id: req.params.id
             })
             .then(data => {
-                res.json(data);
+                console.log(data);
+                res.json({
+                    task: data
+                });
             })
             .catch(err => res.json(err))
     },
-
+    new: function (req, res) {
+        res.json({
+            task: "nothing to add"
+        });
+    },
     create: function (req, res) {
         Task.create(req.body)
             .then(data => {
@@ -30,15 +37,22 @@ module.exports = {
             })
             .catch(err => res.json(err))
     },
-
+    edit: function (req, res) {
+        res.json({
+            task: "nothing to edit"
+        });
+    },
     update: function (req, res) {
-        Task.update(req.params.id, req.params.name)
+        Task.update({
+                _id: req.params.id
+            }, {
+                title: req.body
+            })
             .then(data => {
                 res.redirect('/');
             })
             .catch(err => res.json(err))
     },
-
     delete: function (req, res) {
         Task.remove(req.params.id)
             .then(data => {
